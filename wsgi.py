498561ef4cc67c803bf7ebe0b95db86b990375a8
DeekshithVemula <48466367+DeekshithVemula@users.noreply.github.com>
2019-07-16 18:13:17 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -12,7 +12,6 @@ def hello():
     channel.queue_declare(queue='hello')
 
     channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-    print(" [x] Sent 'Hello World!'")
     connection.close()
     return "Hello World!"
 
