badd7b9494b64491618d078b40e9ca19ebb1d1dd
DeekshithVemula <48466367+DeekshithVemula@users.noreply.github.com>
2019-07-16 17:52:34 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,19 @@
+#!/usr/bin/env python
+import pika
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
+    connection = pika.BlockingConnection(
+         pika.ConnectionParameters(host='rabbitmq-cluster'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
+    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+    print(" [x] Sent 'Hello World!'")
+    connection.close()
     return "Hello World!"
 
 if __name__ == "__main__":
