c30d07756b8310089621f9570645ae4597820103
DeekshithVemula <48466367+DeekshithVemula@users.noreply.github.com>
2019-07-18 12:16:45 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 import pika
+import rabbitmq
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    connection = pika.BlockingConnection(
-         pika.ConnectionParameters(host='rabbitmq-cluster'))
-    channel = connection.channel()
+    credentials = pika.PlainCredentials('test','test')
+    parameters = pika.ConnectionParameters('10.128.1.62',5672,'/',credentials)
+    connection = pika.BlockingConnection(parameters)
+    
 
+    channel = connection.channel()
     channel.queue_declare(queue='hello')
 
     channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+   
     connection.close()
-    return "Hello World!"
+    return "Hello World Hiiii!"
 
 if __name__ == "__main__":
     application.run()
